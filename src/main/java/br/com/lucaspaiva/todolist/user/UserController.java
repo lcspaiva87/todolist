6b09dbc24edd3d07aa9660f33d465676bebf0550
lucas paiva <lcspaiva87@gmail.com>
2023-10-10 18:57:23 -0300

    fix:starting the user interface, installing the lombok lib to make the code more readable in the set and get methods

--- src/main/java/br/com/lucaspaiva/todolist/user/UserController.java
+++ src/main/java/br/com/lucaspaiva/todolist/user/UserController.java
@@ -1,19 +1,27 @@
 package br.com.lucaspaiva.todolist.user;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-          System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
 
-
-     
+           if(user != null){
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(400).body("Usuário já existe");
+        }
+        var useCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(201).body(useCreated);  
     }
 }
--- src/main/java/br/com/lucaspaiva/todolist/user/UserModel.java
+++ src/main/java/br/com/lucaspaiva/todolist/user/UserModel.java
@@ -1,9 +1,65 @@
 package br.com.lucaspaiva.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+/*
+ * @Data é uma anotação do lombok que gera automaticamente
+ * 
+ * @Getter apenas os getters
+ * 
+ * @Setters apenas os setters
+ * a lib lombok é uma lib que gera getters e setters automaticamente
+ * deixando o codigo mais limpo e facil de ler
+ * 
+ * public void setName(String name) {
+ * this.name = name;
+ * }
+ * 
+ * public String getName() {
+ * return name;
+ * }
+ * 
+ * public void setUsername(String username) {
+ * this.username = username;
+ * }
+ * 
+ * public String getUsername() {
+ * return username;
+ * }
+ * 
+ * public void setPassword(String password) {
+ * this.password = password;
+ * }
+ * 
+ * public String getPassword() {
+ * return password;
+ * }
+ * ele automaticamente gera os getters e setters
+ * 
+ */
+// chave primaria da tabela
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
-    public String username;
-    public String name;
-    public String password;
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
