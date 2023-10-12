d2172c0d7704895295c0a389ea85b964808b7daf
lucas paiva <lcspaiva87@gmail.com>
2023-10-11 21:12:15 -0300

    fix:Create taskModel

---
+++ src/main/java/br/com/lucaspaiva/todolist/filter/OncePerRequestFilter.java
@@ -0,0 +1,5 @@
+package br.com.lucaspaiva.todolist.filter;
+
+public interface OncePerRequestFilter {
+
+}
--- src/main/java/br/com/lucaspaiva/todolist/user/UserController.java
+++ src/main/java/br/com/lucaspaiva/todolist/user/UserController.java
@@ -1,10 +1,13 @@
 package br.com.lucaspaiva.todolist.user;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 
 @RestController
@@ -21,6 +24,8 @@ public class UserController {
             System.out.println("Usuário já existe");
             return ResponseEntity.status(400).body("Usuário já existe");
         }
+        var PasswordHashred =BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(PasswordHashred.toString());
         var useCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(201).body(useCreated);  
     }
